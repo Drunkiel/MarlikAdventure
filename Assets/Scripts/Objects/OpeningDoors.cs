29a6e6e1b247b23650452474dbff71caa5bf8317
Drunkiel <drunkielfel@gmail.com>
2022-01-09 15:35:30 +0100

    Added working doors

--- Assets/Scripts/Objects/OpeningDoors.cs
+++ Assets/Scripts/Objects/OpeningDoors.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OpeningDoors : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Transform Doors;
+    public GameObject Trigger;
+    public float Speed;
+    public bool isTriggered;
+
+    private float StartPlace;
+
+    void Start(){
+        Doors = this.transform;
+        StartPlace = Doors.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isTriggered = Trigger.GetComponent<TriggerController> ().isTriggered;
+        if(isTriggered){
+            OpenDoor();
+        }   else{
+            CloseDoor();
+        }
+    }
+
+    void OpenDoor(){
+        if(Doors.position.x <= StartPlace + 2){
+             
+            Doors.Translate(new Vector2(1 * Speed * Time.deltaTime, 0));
+        }
+    }
+
+    void CloseDoor(){
+        if(Doors.position.x >= StartPlace){
+            Doors.Translate(new Vector2(-1 * Speed * Time.deltaTime, 0));
+        }
     }
 }
