40b6895ba329c0da6459287e3e10413aa17f06d0
Drunkiel <drunkielfel@gmail.com>
2022-01-11 16:03:30 +0100

    Added StatsController

--- Assets/Scripts/Player/StatsController.cs
+++ Assets/Scripts/Player/StatsController.cs
@@ -1,6 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationController : MonoBehaviour
+public class StatsController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
