5c99a2a405d3dd08e9cb7a5c04477daf97cf11a3
Drunkiel <drunkielfel@gmail.com>
2022-01-13 10:31:26 +0100

    Edited Animation controller

--- Assets/Scripts/Player/AnimationController.cs
+++ Assets/Scripts/Player/AnimationController.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
+    private Animator Anim;
+    Vector2 Move;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Anim = GetComponent<Animator> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Animating();
+    }
+
+    void Animating(){
+        //Inicjalizing inputs
+        Move.x = Input.GetAxis("Horizontal"); 
+        Move.y = Input.GetAxis("Vertical");
+
+        Anim.SetFloat("Horizontal", Move.x);
+        Anim.SetFloat("Vertical", Move.y);
+        Anim.SetFloat("Speed", Move.sqrMagnitude);
     }
 }
