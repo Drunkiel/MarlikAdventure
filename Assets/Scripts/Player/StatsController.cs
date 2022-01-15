84b083afddd3fc9a3beadfb887edc420be8ff1f4
Drunkiel <drunkielfel@gmail.com>
2022-01-15 09:35:03 +0100

    Added new stuff

--- Assets/Scripts/Player/StatsController.cs
+++ Assets/Scripts/Player/StatsController.cs
@@ -1,13 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StatsController : MonoBehaviour
 {
+    public float Health;
+    private float MaxHealth;
+    public int Level;
+    public float Experience;
+    public float ExpToNextLvl;
+    public int Cash;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MaxHealth = Health;
     }
 
     // Update is called once per frame
