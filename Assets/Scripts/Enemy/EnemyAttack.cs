3ca0a7591d5d488cf19d378202952591f4266f6b
Drunkiel <drunkielfel@gmail.com>
2022-01-28 14:55:03 +0100

    Added working attack script

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -4,27 +4,46 @@ public class EnemyAttack : MonoBehaviour
 {
     public bool isTriggered;
     public float Damage;
+    public float AttackRange;
 
     public float Cooldown;
+    private float ResCooldown;
+
+    public Transform AttackPoint;
+    public LayerMask PlayerLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResCooldown = Cooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
         isTriggered = transform.GetChild(0).GetComponent<TriggerController> ().isTriggered;
+
+        if(isTriggered){
+            Attack();
+        }   else{
+            Cooldown = ResCooldown;
+        }
     }
 
     void Attack(){
-        if(Cooldown <= 0){      
+        //Detecting player
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, PlayerLayer);
+        
+        //Do damage to player
+        foreach(Collider2D player in hitPlayer){
+            if(Cooldown <= 0){      
+                player.GetComponent<StatsController>().TakeDamage(Damage);               
+                Cooldown = ResCooldown;
 
-        }   else{
+            }   else{
 
-            Cooldown -= Time.deltaTime;
+                Cooldown -= Time.deltaTime;
+            }
         }
     }
 }
