46098aa920bcf522858e1da492ad5038d95959a0
Drunkiel <drunkielfel@gmail.com>
2022-02-04 17:45:38 +0100

    Added functionality

--- Assets/Scripts/Objects/LanternController.cs
+++ Assets/Scripts/Objects/LanternController.cs
@@ -5,7 +5,9 @@ public class LanternController : MonoBehaviour
     public GameObject Light;
 
     public bool isTurnedOn;
+    public bool isTriggered;
 
+    private float Cooldown = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,25 @@ public class LanternController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isTriggered = GetComponent<TriggerController> ().isTriggered;
+
+        if(isTriggered){
+
+            TimerToTurnOn();
+        }   else{
+
+            Cooldown = 5f;
+        }
+    }
+
+    void TimerToTurnOn(){
+        if(Cooldown <= 0){      
+
+            Light.SetActive(true);
+            isTurnedOn = true;
+        }   else{
+
+            Cooldown -= Time.deltaTime;
+        }
     }
 }
