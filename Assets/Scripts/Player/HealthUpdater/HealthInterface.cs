52aeff335a33bf7cc2d08579a0062aebcf039c81
Drunkiel <drunkielfel@gmail.com>
2022-02-04 13:48:56 +0100

    Refactoring

--- Assets/Scripts/Player/HealthUpdater/HealthInterface.cs
+++ Assets/Scripts/Player/HealthUpdater/HealthInterface.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public interface HealthInterface {
     void HeartUpdate(Slider HealthBar, Image FillBar, float MaxHealth, float Health, Color FullHp, Color HalfHp, Color QuartHp);
 }
