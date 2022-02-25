b654a604692e11aac71122f047e06cc4e7415fc9
Drunkiel <drunkielfel@gmail.com>
2022-02-25 11:33:30 +0100

    Added FixedUpdate state to use Checker()

--- Assets/Scripts/Objects/MapLocalizationController.cs
+++ Assets/Scripts/Objects/MapLocalizationController.cs
@@ -5,7 +5,12 @@ public class MapLocalizationController : MonoBehaviour
     public GameObject[] Maps;
     public bool[] isPlayerThere;
 
-    public void Checker(){
+    void FixedUpdate()
+    {
+        Checker();
+    }
+
+    void Checker(){
         for (int i = 0; i < isPlayerThere.Length; i++)
         {
             isPlayerThere[i] = Maps[i].GetComponent<TriggerController> ().isTriggered;
