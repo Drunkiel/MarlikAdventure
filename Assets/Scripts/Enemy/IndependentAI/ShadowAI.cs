716130cb8f934e593246626674d38e05805c40c1
Drunkiel <drunkielfel@gmail.com>
2022-03-11 09:54:26 +0100

    Refactoring code

--- Assets/Scripts/Enemy/IndependentAI/ShadowAI.cs
+++ Assets/Scripts/Enemy/IndependentAI/ShadowAI.cs
@@ -7,12 +7,6 @@ public class ShadowAI : MonoBehaviour
 
     public LayerMask LayerToSearch;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -29,16 +23,6 @@ public class ShadowAI : MonoBehaviour
 
     void Death(){
         Destroy(this.gameObject, 0.5f);
-        isLightNearby = false;
-    }
-
-    void RandomTp(){
-        Transform bodyPos = GetComponent<Transform> ();
-
-        float RandomXPos = Mathf.Round(Random.Range(bodyPos.position.x - 5, bodyPos.position.x + 5));
-        float RandomYPos = Mathf.Round(Random.Range(bodyPos.position.y - 5, bodyPos.position.y + 5));
-
-        bodyPos.position = new Vector3(RandomXPos, RandomYPos, 0);
         isLightNearby = false;
     }
 
