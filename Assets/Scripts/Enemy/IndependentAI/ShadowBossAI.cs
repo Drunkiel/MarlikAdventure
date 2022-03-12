e08947e43f9d07642ffb0c8f5a16f476063055d2
Drunkiel <drunkielfel@gmail.com>
2022-03-12 09:51:57 +0100

    Added functionality to spawn Shadows

--- Assets/Scripts/Enemy/IndependentAI/ShadowBossAI.cs
+++ Assets/Scripts/Enemy/IndependentAI/ShadowBossAI.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class ShadowBossAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Shadow;
+    private float AmountOfMobs;
+
+    public float Cooldown;
+    public float ResCooldown;
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpawnMobs();
+    }
+
+    void SpawnMobs(){
+        if(Cooldown <= 0){      
+
+            Cooldown = ResCooldown;
+            AmountOfMobs = Mathf.Round(Random.Range(1, 4));
+
+            for (int i = 0; i < AmountOfMobs; i++)
+            {
+                Instantiate(Shadow, transform.position, Quaternion.identity);
+            }
+        }   else{
+
+            Cooldown -= Time.deltaTime;
+        }
     }
 }
