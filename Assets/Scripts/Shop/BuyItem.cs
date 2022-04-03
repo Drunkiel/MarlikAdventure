bbbd4e50875e6f2487d523612b747d5e690a1043
Drunkiel <drunkielfel@gmail.com>
2022-04-03 09:36:16 +0200

    Some upgrades

--- Assets/Scripts/Shop/BuyItem.cs
+++ Assets/Scripts/Shop/BuyItem.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuyItem : MonoBehaviour
 {
     public GameObject Item;
     public int Cost;
+    public Image Image;
+
+    void Start()
+    {
+        Cost = Item.GetComponent<ItemDesc> ().Cost;   
+        Image.sprite = Item.GetComponent<SpriteRenderer> ().sprite;
+    }
 
     public void Buy(){
         int PlayerCash = GameObject.FindGameObjectWithTag("Player").GetComponent<StatsController> ().Cash;
         
         if(PlayerCash >= Cost){
             SpawnItem Spawn = GameObject.FindGameObjectWithTag("Shop").GetComponent<SpawnItem> ();
+            PlayerCash -= Cost;
             Spawn.SpawnDroppedItem(Item);
         }
     }
