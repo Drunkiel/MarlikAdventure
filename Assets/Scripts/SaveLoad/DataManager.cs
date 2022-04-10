98caa2bf34b38f096a2391fd6c946b836d2f446e
Drunkiel <drunkielfel@gmail.com>
2022-04-10 15:43:29 +0200

    Now can load inventory

--- Assets/Scripts/SaveLoad/DataManager.cs
+++ Assets/Scripts/SaveLoad/DataManager.cs
@@ -24,6 +24,6 @@ public class DataManager : MonoBehaviour
     }
 
     public void LoadGame(){
-        
+        itemsData.LoadItems();
     }
 }
--- Assets/Scripts/SaveLoad/ItemsToSave.cs
+++ Assets/Scripts/SaveLoad/ItemsToSave.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ItemsToSave : MonoBehaviour
 {
     DataToSave gameData;
+    public GameObject[] allGameItems;
     private GameObject[] invSlots;
 
     // Start is called before the first frame update
@@ -17,6 +18,19 @@ public class ItemsToSave : MonoBehaviour
         {
             if(invSlots[i].transform.childCount > 0){
                 gameData.inventoryItems[i] = invSlots[i].GetComponentInChildren<SpawnItem> ().Item.GetComponent<ItemDesc> ().Id;
+            }
+        }
+    }
+
+    public void LoadItems(){
+        for (int i = 0; i < invSlots.Length; i++)
+        {
+            if(invSlots[i].transform.childCount > 0){
+                Destroy(invSlots[i].transform.GetChild(0).gameObject);
+            }
+
+            if(gameData.inventoryItems[i] != 0){
+                Instantiate(allGameItems[gameData.inventoryItems[i]], invSlots[i].transform, false);
             }
         }
     }
