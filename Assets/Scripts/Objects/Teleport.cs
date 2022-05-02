b2d6a34e0991cc464c1a12510d7266d98ec0f422
Drunkiel <drunkielfel@gmail.com>
2022-05-02 18:02:42 +0200

    Now can teleport between scenes

--- Assets/Scripts/Objects/Teleport.cs
+++ Assets/Scripts/Objects/Teleport.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teleport : MonoBehaviour
 {
-    public Transform pointToTeleport;
+    #nullable enable
+    public bool sceneTeleport;
+    public string? sceneName;
+    public Transform? pointToTeleport;
 
     public float cooldown;
     public bool isTriggered;
+    
+    #nullable disable
+    public SaveLoad saveLoad;
 
     // Update is called once per frame
     void Update()
@@ -20,9 +27,14 @@ public class Teleport : MonoBehaviour
     }
 
     void Teleportation(){
-        if(cooldown <= 0){      
-            GameObject PlayerPos = GameObject.FindGameObjectWithTag("Player");
-            PlayerPos.transform.position = pointToTeleport.position;
+        if(cooldown <= 0){    
+            if(!sceneTeleport && pointToTeleport != null){
+                GameObject PlayerPos = GameObject.FindGameObjectWithTag("Player");
+                PlayerPos.transform.position = pointToTeleport.position;
+            }   else{
+                saveLoad.Save();
+                SceneManager.LoadScene(sceneName);
+            }
 
             cooldown =  5;
         }   else{
